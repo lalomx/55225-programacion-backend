fde82af6872cbdb2e135eb87fe5c7750a6d0c489
Lalo Velazquez <lalovelazquez@MacBook-Pro-de-Eduardo.local>
2023-06-06 17:34:32 -0600

    primera clase

---
+++ clases/00/index.js
@@ -0,0 +1,51 @@
+let nombre = 'Federico' // string
+let apellido = 'Iramain'
+let edad = 32 // number
+let esMayorDeEdad = true // boolean
+let nombreDeMascota = undefined // undefined
+let tieneAuto = null // null
+const amigos = ['Lalo', 'Mariano'] // array index 0
+const direccion = { calle: 'Santa Monica', num: 10, barrio: 'Santa Monica', ciudad: 'Queretaro' } // obj
+
+
+
+
+// Date
+// RegExp
+// Buffer
+// process
+// global
+
+// eslint 
+
+// console.log(nombre)
+
+// nombre = 'Gabriel'
+
+// console.log(nombre)
+
+// console.log(direccion)
+
+// direccion.barrio = 'Carrillo'
+
+// console.log(direccion)
+
+console.log(amigos)
+
+amigos[0] = 'Nahuel'
+
+console.log(amigos)
+
+amigos[1] = 'Fabrizio'
+
+console.log(amigos)
+
+// function suma(a, b) {
+//   return a + b
+// }
+
+// I/O -> async
+
+const suma = (a, b) => a + b
+
+console.log(suma(2, 3))
