36d0b44231cdd4caf3fb3e0fbeb6b8dccad10e85
Lalo Velazquez <evelazquez@avionica.com>
2023-08-10 16:06:00 -0400

    base clase 17

---
+++ clases/17/routes/home.router.js
@@ -0,0 +1,71 @@
+const { Router } = require('express')
+const path = require('path')
+const productManager = require('../managers/product.manager')
+
+const router = Router()
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+router.get('/', async (req, res) => {
+  // res.sendFile(path.join(__dirname, '../public/index.html'))
+  const { page = 1, size = 5 } = req.query
+  const { docs: products, ...pageInfo } = await productManager.getAllPaged(page, size)
+
+  pageInfo.prevLink = pageInfo.hasPrevPage ? `http://localhost:3000/?page=${pageInfo.prevPage}&size=${size}` : ''
+  pageInfo.nextLink = pageInfo.hasNextPage ? `http://localhost:3000/?page=${pageInfo.nextPage}&size=${size}` : ''
+
+  console.log("ID del product manager desde home router", productManager.id)
+
+  console.log(pageInfo)
+
+  res.render('home', {
+    title: 'Home',
+    products,
+    pageInfo,
+    user: req.user ?  {
+      ...req.user,
+      isAdmin: req.user?.role == 'admin',
+    } : null,
+    style: 'home'
+  })
+})
+
+router.get('/chat', (req, res) => {
+  res.render('chat')
+})
+
+router.get('/realtimeproducts', async (req, res) => {
+  // res.sendFile(path.join(__dirname, '../public/index.html'))
+  const products = await productManager.getAll()
+  // const randomId = getRandomNumber(0, products.length - 1)
+
+  res.render('realTimeProducts', {
+    title: 'Real Time',
+    products,
+    user: {
+      ...req.user,
+      isAdmin: req.user.role == 'admin',
+    },
+    style: 'home'
+  })
+})
+
+router.get('/carrito', (req, res) => {
+  // res.sendFile(path.join(__dirname, '../public/carrito.html'))
+  // interactuar con el manager de carrito
+  res.render('carrito', {
+    numItems: 2,
+    title: 'Carrito'
+  })
+})
+
+router.get('/login', (_, res) => res.render('login'))
+router.post('/login', (req, res) => {
+  console.log(req.body)
+
+  res.redirect('/')
+})
+
+module.exports = router
