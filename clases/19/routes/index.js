aa391e623aa0701b215571fb5a0d78c93a7a36dc
Lalo Velazquez <evelazquez@avionica.com>
2023-08-14 21:35:33 -0400

    Base clase 19

---
+++ clases/19/routes/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const ProductRoutes = require('./api/products.router.js')
+const UserRoutes = require('./api/users.router.js')
+const HomeRoutes = require('./home.router.js')
+const AdminRoutes = require('./admin.router.js')
+
+const api = Router();
+
+api.use('/products', ProductRoutes);
+api.use('/users', UserRoutes);
+
+const home = Router()
+
+home.use('/', HomeRoutes)
+home.use('/admin', AdminRoutes)
+
+module.exports = {
+  api,
+  home
+};
