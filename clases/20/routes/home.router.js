eee7d4ce40c4a715cb206d2cfe31e3de7af18217
Lalo Velazquez <evelazquez@avionica.com>
2023-08-16 22:36:09 -0400

    base clase 20

--- clases/20/routes/home.router.js
+++ clases/20/routes/home.router.js
@@ -35,7 +35,12 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/chat', isAuth, (req, res) => {
-  res.render('chat')
+  res.render('chat', { 
+      user: req.user ?  {
+      ...req.user,
+      isAdmin: req.user?.role == 'admin',
+    } : null,
+  })
 })
 
 router.get('/realtimeproducts', async (req, res) => {
@@ -58,148 +63,22 @@ router.get('/carrito', (req, res) => {
   // interactuar con el manager de carrito
   res.render('carrito', {
     numItems: 2,
-    title: 'Carrito'
+    title: 'Carrito',
+    user: req.user ?  {
+      ...req.user,
+      isAdmin: req.user?.role == 'admin',
+    } : null,
   })
 })
 
 router.get('/profile', isAuth, (req, res) => {
   res.render('profile', {
-    ...req.session.user
+    user: req.user ?  {
+      ...req.user,
+      isAdmin: req.user?.role == 'admin',
+    } : null,
   })
 })
-router.get('/signup', (_, res) => res.render('signup'))
-router.post('/signup', async (req, res) => {
-  const user = req.body
-  
-  console.log(user)
 
-  const existing = await userManager.getByEmail(user.email)
-
-  if (existing) {
-    return res.render('signup', {
-      error: 'El email ya existe'
-    })
-  }
-
-  // crear al usuario
-  try {
-    const newUser = await userManager.create({
-      ...user,
-      password: hashPassword(user.password),
-    })
-
-    req.session.user = {
-      name: newUser.firstname,
-      id: newUser._id,
-      ...newUser._doc
-    }
-
-    console.log(req.session)
-
-    req.session.save((err) => {
-      res.redirect('/')
-    })
-
-  } catch(e) {
-    return res.render('signup', {
-      error: 'Ocurrio un error. Intentalo mas tarde'
-    })
-  }
-
-  
-
-})
-
-router.get('/login', (_, res) => res.render('login'))
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body
-
-  try {
-
-    const user = await userManager.getByEmail(email)
-
-    console.log(user.password, password)
-
-    if (!user) {
-      return res.render('login', { error: 'El usuario no existe' })
-    }
-
-    if (!user.password || !password || !isValidPassword(password, user.password)) {
-      return res.render('login', { error: 'Contraseña invalida' })
-    }
-
-    req.session.user = {
-      name: user.firstname,
-      id: user._id,
-      // role: 'Admin'
-      ...user
-    }
-
-    req.session.save((err) => {
-      if(!err) {
-        res.redirect('/')
-      }
-    })
-  } catch(e) {
-    console.log(e)
-    res.render('login', { error: 'Ha ocurrido un error' })
-  }
-
-  // guardo la session con la informacion del usuario
-})
-router.get('/logout', isAuth, (req, res) => {
-  const { user } = req.cookies
-
-  // borrar la cookie
-  res.clearCookie('user')
-
-  req.session.destroy((err) => {
-    if(err) {
-      return res.redirect('/error')
-    }
-
-    res.render('logout', {
-      user: req.user.name
-    })
-
-    req.user = null
-  })
-
-  // res.render('logout', {
-  //   user
-  // })
-})
-
-router.get('/resetpassword', (_, res) => res.render('resetpassword'))
-router.post('/resetpassword', async (req, res) => {
-
-  const { email, password1, password2 } = req.body
-
-  const user = await userManager.getByEmail(email)
-
-  if (!user) {
-    return res.render('resetpassword', { error: 'El usuario no existe' })
-  }
-
-  if(password1 !== password2) {
-    return res.render('resetpassword', { error: 'Las contraseñas no coinciden' })
-  }
-
-  try {
-    
-    await userManager.save(user._id, {
-      ...user,
-      password: hashPassword(password1)
-    })
-
-    res.redirect('/login')
-
-  } catch(e) {
-    console.log(e)
-    return res.render('resetpassword', { error: 'Ha ocurrido un error' })
-  }
-})
 
 module.exports = router
-
-// a cipher is a two-way operation, whereas hashing is a one-way operation
