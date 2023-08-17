f70b47bfc5704af4c41a84f2a5ddb216f48e1a77
lalomx <lalo0892@gmail.com>
2023-08-17 16:50:42 -0600

    primera parte

--- clases/20/routes/index.js
+++ clases/20/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const ProductRoutes = require('./api/products.router.js')
 const UserRoutes = require('./api/users.router.js')
 const HomeRoutes = require('./home.router.js')
+const LoginRoutes = require('./login.router.js')
 const AdminRoutes = require('./admin.router.js')
 
 const api = Router();
@@ -12,6 +13,8 @@ api.use('/users', UserRoutes);
 const home = Router()
 
 home.use('/', HomeRoutes)
+home.use('/', LoginRoutes)
+
 home.use('/admin', AdminRoutes)
 
 module.exports = {
