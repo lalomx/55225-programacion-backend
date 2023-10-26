8907a299ddbe29f29be4de7d03c1898671282cae
lalomx <lalo0892@gmail.com>
2023-10-26 18:14:13 -0400

    clase 70

--- clases/40/middlewares/logger.middleare.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../logger')
-
-const fn = (req, _res, next) => {
-  logger.http(`[${req.method}] - ${req.url} at ${(new Date()).toISOString()}`)
-  next()
-}
-
-module.exports = fn
